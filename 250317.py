b62d29f60ca96cd018c042971ae4e62318912b2d
kimhyengyu <sisalbkim@gmail.com>
2025-03-17 11:30:45 +0900

    Add files via upload

---
+++ 250317.py
@@ -0,0 +1,28 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var1=input()
+100
+>>> print(var1)
+100
+>>> var2=input()
+200
+>>> print(var2)
+200
+>>> print(var1 + var2)
+100200
+>>> print(var1+var2)
+100200
+>>> var1=int(input())
+100
+>>> var2=int(input())
+200
+>>> print(var1+var2)
+300
+>>> name = input("이름을 입력하세요 ==>")
+이름을 입력하세요 ==>김현규
+>>> print(name)
+김현규
+>>> 
+>>> a,b = 100,200
+>>> print(a==b,a!=b,a<b,a>b,a>=b,a<=b)
+False True True False False True
---
+++ age.py
@@ -0,0 +1,6 @@
+age = 10
+print(age)
+age = 20
+print(age)
+
+type(age)
